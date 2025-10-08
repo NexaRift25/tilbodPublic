2fa653e58b8dc66176f41058870563ef3a4993c5
Md. Mosaraf Hossain Nissan <151007488+nissandev@users.noreply.github.com>
2025-10-08 22:00:59 +0600

    right arrow icon change to svg

--- components/ui/AnimatedButton.tsx
+++ components/ui/AnimatedButton.tsx
@@ -1,19 +1,30 @@
 import { MoveRight } from "lucide-react";
+import Image from "next/image";
 
 const AnimatedButton = () => {
   return (
     <button className="group  whitespace-nowrap overflow-hidden  w-full py-[0.6875rem] px-[1.5rem] rounded-[3.5rem] bg-card-background border border-primary hover:border-green-500 transition-all duration-100 cursor-pointer text-primary relative">
       <div className="flex items-center justify-between -translate-x-1/4 transition-all duration-400 group-hover:translate-x-0">
-        <span>
-          <MoveRight size={32} className="text-green-500" />
-        </span>
-
+        <Image
+          src="/rightArrow.svg"
+          alt="arrow-right"
+          width={32}
+          height={32}
+          className="w-fit object-cover"
+          style={{ filter: "invert(49%) sepia(97%) saturate(749%) hue-rotate(86deg) brightness(92%) contrast(88%)" }}
+        />
         <div className="">
           <span className="text-xs sm:text-sm md:text-base lg:text-2xl font-semibold">
             View offer
           </span>
         </div>
-        <MoveRight size={32} className="w-fit" />
+        <Image
+          src="/rightArrow.svg"
+          alt="arrow-right"
+          width={32}
+          height={32}
+          className="w-fit object-cover"
+        />
       </div>
     </button>
   );
