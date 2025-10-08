786cd07e4e75e93fc3045c69b0626e0dd2c0b31f
Md. Mosaraf Hossain Nissan <151007488+nissandev@users.noreply.github.com>
2025-10-08 21:48:37 +0600

    enimation button correction

--- components/ui/AnimatedButton.tsx
+++ components/ui/AnimatedButton.tsx
@@ -1,24 +1,22 @@
 import { MoveRight } from "lucide-react";
 
 const AnimatedButton = () => {
-    return (
-        <button className="hidden whitespace-nowrap md:flex items-center overflow-hidden animated_button w-full py-[0.6875rem] px-[1.5rem] rounded-[3.5rem]  gap-[4.375rem] bg-card-background border border-primary hover:border-green-500 transition-all duration-100 cursor-pointer text-primary relative">
-            <span className="button_icon">
-                <MoveRight color="green" size={25} className="" />
-            </span>
+  return (
+    <button className="group  whitespace-nowrap overflow-hidden  w-full py-[0.6875rem] px-[1.5rem] rounded-[3.5rem] bg-card-background border border-primary hover:border-green-500 transition-all duration-100 cursor-pointer text-primary relative">
+      <div className="flex items-center justify-between -translate-x-1/4 transition-all duration-400 group-hover:translate-x-0">
+        <span>
+          <MoveRight size={32} className="text-green-500" />
+        </span>
 
-            <div className="animated_button_content flex items-center w-full">
-                <span className="text-xs sm:text-sm md:text-base lg:text-2xl font-semibold mr-[100px]">
-                    View offer
-                </span>
-                <MoveRight size={26} className="w-6 h-6" />
-            </div>
-        </button>
-    );
+        <div className="">
+          <span className="text-xs sm:text-sm md:text-base lg:text-2xl font-semibold">
+            View offer
+          </span>
+        </div>
+        <MoveRight size={32} className="w-fit" />
+      </div>
+    </button>
+  );
 };
 
 export default AnimatedButton;
-
-
-
-
