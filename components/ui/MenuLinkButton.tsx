a6daad08a60aec50a6e3aa957ba5ebfeab16160f
Md. Mosaraf Hossain Nissan <151007488+nissandev@users.noreply.github.com>
2025-10-09 13:54:33 +0600

    header responsived

---
+++ components/ui/MenuLinkButton.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+export default function MenuLinkButton({
+  nav,
+}: {
+  nav: { title: string; link: string };
+}) {
+  return (
+    <div>
+      <div className="line_button relative flex cursor-pointer items-center gap-6 text-white transition-colors hover:text-primary xl:pr-[137px]">
+        <div className="line_button_content font-semibold text-2xl">
+          {nav.title}
+        </div>
+
+        <Image
+          src="/rightArrow.svg"
+          alt="arrow-right"
+          width={32}
+          height={32}
+          className="w-fit object-cover"
+        />
+
+        <div className="bottom_line absolute h-[3px] bottom-[-8px] w-[32%] md:w-[40%] hover:w-fit bg-primary" />
+      </div>
+    </div>
+  );
+}
