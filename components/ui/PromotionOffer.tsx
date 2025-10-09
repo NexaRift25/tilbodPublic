af266e7d587770d027dcbb44c87849f4fc2c7e7d
Md. Mosaraf Hossain Nissan <151007488+nissandev@users.noreply.github.com>
2025-10-09 15:56:45 +0600

    removed padding

--- components/ui/PromotionOffer.tsx
+++ components/ui/PromotionOffer.tsx
@@ -3,7 +3,7 @@ import PromoCardWraper from "@/componentWraper/PromoCardWraper";
 export default function PromotionOffer() {
     return (
         <div>
-            <div className="w-full max-w-[118.75rem] mx-auto pl-4 sm:pl-6 lg:pl-8">
+            <div className="w-full max-w-[118.75rem] mx-auto">
                 <PromoCardWraper />
             </div>
         </div>
