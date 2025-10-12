d515b31150f53e95ee552283d900022372d00afc
Md. Mosaraf Hossain Nissan <151007488+nissandev@users.noreply.github.com>
2025-10-12 11:05:02 +0600

    view offer card logic build

--- components/ui/ViewMoreOffers.tsx
+++ components/ui/ViewMoreOffers.tsx
@@ -1,34 +1,45 @@
+import { usePathname } from "next/navigation";
 import Container from "./Container";
 import MenuLinkButton from "./MenuLinkButton";
 
 export default function ViewMoreOffers() {
+  const pathname = usePathname();
+  const offerPaths = [
+    {
+      path: "/active-offers",
+      title: "Active offers",
+    },
+    {
+      path: "/weekday-specials",
+      title: "Weekday deals",
+    },
+    {
+      path: "/happy-hour-offers",
+      title: "Happy hour offers",
+    },
+    {
+      path: "/gift-certificates",
+      title: "Gift certificate",
+    },
+  ];
+
   return (
     <Container className="max-w-[118.75rem] w-[85%]">
       <div className="my-4 p-16 space-y-12 sm:my-6 lg:my-8 rounded-[3rem] border border-solid border-primary bg-card-background">
         <h2 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-semibold text-smoky-white">
           View More Offers
         </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 lg:w-[90%] 2xl:w-[70%]">
-          <MenuLinkButton
-            nav={{ title: "Active offers", link: "/active-offers" }}
-            isActive={false}
-            onClick={() => {}}
-          />
-          <MenuLinkButton
-            nav={{ title: "Weekday deals", link: "/weekday-specials" }}
-            isActive={false}
-            onClick={() => {}}
-          />
-          <MenuLinkButton
-            nav={{ title: "Happy hour offers", link: "/happy-hour-offers" }}
-            isActive={false}
-            onClick={() => {}}
-          />
-          <MenuLinkButton
-            nav={{ title: "Gift certificate", link: "/gift-certificates" }}
-            isActive={false}
-            onClick={() => {}}
-          />
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 lg:w-[90%] 2xl:w-[70%]">
+          {offerPaths.map(
+            (path) =>
+              path.path !== pathname && (
+                <MenuLinkButton
+                  nav={{ title: path.title, link: path.path }}
+                  isActive={false}
+                  onClick={() => {}}
+                />
+              )
+          )}
         </div>
       </div>
     </Container>
